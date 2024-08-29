d70138a78105efd8be1f96d316e2046a2d580075
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-08-30 00:21:12 +0530

    Missing Number (CSES)

---
+++ Introductory Problems/MissingNumber.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    long long n, val;
+    cin >> n;
+    long long sum = 0;
+    for(int i=0; i<n-1; i++){
+        cin >> val;
+        sum += val;
+    }
+    long long orgSum = (n*(n+1))/2;
+    cout << orgSum - sum << endl;
+}
